371668d66b0b447efd62c50734d94d375616bbde
sunbo.kie <sunbo.kie@bytedance.com>
2023-03-30 11:14:32 +0000

    [ADD] add some basic page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import VoyageView from '@/views/Voyage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/:pathMatch(.*)*',
+      name: '404 Not Found',
+      redirect: { name: 'voyage' }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:"/voyage",
+      name: "voyage",
+      component: VoyageView
     }
   ]
 })
---
+++ src/views/Voyage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col h-screen bg-black">
+        <h1 class="text-red-700">hello</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
